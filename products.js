ea6c3bd425f7864b8fa5e4f8c6a5e131d9231f7d
Abhi30121998 <griddaluriabihi24@gmail.com>
2025-04-23 04:03:39 +0000

    This is my submission

--- products.js
+++ products.js
@@ -1,13 +1,39 @@
 const fs = require('fs').promises
 const path = require('path')
+const cuid = require('cuid')
+const db = require('./db')
+const { title } = require('process')
 
 const productsFile = path.join(__dirname, 'data/full-products.json')
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: { type: String },
+  alt_description: { type: String },
+  likes: { type: Number, required: true },
+  urls: {
+    regular: { type: String, required: true },
+    small: { type: String, required: true },
+    thumb: { type: String, required: true },
+  },
+  links: {
+    self: { type: String, required: true },
+    html: { type: String, required: true },
+  },
+  user: {
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    portfolio_url: { type: String },
+    username: { type: String, required: true },
+  },
+  tags: [{
+    title: { type: String, required: true },
+  }], 
+})
 
 /**
  * List products
- * @param {*} options 
- * @returns 
- */
+@@ -11,39 +39,55 @@ const productsFile = path.join(__dirname, 'data/full-products.json')
 async function list(options = {}) {
 
   const { offset = 0, limit = 25, tag } = options;
@@ -17,11 +43,24 @@ async function list(options = {}) {
     .filter(product => {
       if (!tag) {
         return product
+  const query = tag ? {
+    tags: {
+      $elemMatch: {
+        title: tag
       }
+    }
+  } : {}
+
+  const products= await Product.find(query)
+    .sort(({_id:1}))
+    .skip(offset)
+    .limit(limit)
 
       return product.tags.find(({ title }) => title == tag)
     })
     .slice(offset, offset + limit) // Slice the products
+    return products
+
 }
 
 /**
@@ -31,6 +70,14 @@ async function list(options = {}) {
  */
 async function get(id) {
   const products = JSON.parse(await fs.readFile(productsFile))
+async function get(_id) {
+  const product = await Product.findById(_id)
+  return product
+}
+async function create (fields){
+  const product = await new Product(fields).save()
+  return product
+}
 
   // Loop through the products and return the product with the matching id
   for (let i = 0; i < products.length; i++) {
@@ -38,12 +85,28 @@ async function get(id) {
       return products[i]
     }
   }
+async function edit (_id,change){
+  const product=await get(_id)
+  Object.keys(change).forEach(function(key){
+    product[key] =change[key]
+  })
+  //Object.assign(product,{ ...change});
 
   // If no product is found, return null
   return null;
 }
+  await product.save()
+  return product
+ }
 
+ async function destroy(_id){
+  return await Product.deleteOne({_id})
+ }
 module.exports = {
   list,
   get
+  get,
+  create,
+  edit,
+  destroy
 }
